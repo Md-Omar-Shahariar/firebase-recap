e5f81ccd3b45690a01009de30146f01944f30964
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-04-10 17:27:15 +0600

    sign out

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log(result);
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {});
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
       })
       .catch((error) => {
-        console.log(error.message);
+        console.log(error);
       });
   };
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google</button>
+      <h2>Name: {user.displayName}</h2>
+      <h3>Your Email : {user.email} </h3>
+      <h4>{user.providerId}</h4>
+
+      <button onClick={handleSignOut}>Sign Out</button>
     </div>
   );
 }
